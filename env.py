f980325f37843df9947ebc813dda7dd059173126
Stien Vanderhallen <stien.vanderhallen@hotmail.com>
2021-03-11 09:19:33 +0100

    Delete env.py

--- env.py
+++
@@ -1,9 +0,0 @@
-config = {}
-
-config['WEBEX_BASE_URL'] = "https://webexapis.com"
-config['WEBEX_ACCESS_TOKEN'] = "MzY3NjQ2NGEtMWE3MC00ZWJlLTlhMTktYjRjZjM5NTdjZGNhOTJhODllODktZThm_PF84_1eb65fdf-9643-417f-9974-ad72cae0e10f"
-config['PRODUCTION_ROOM'] = "Y2lzY29zcGFyazovL3VzL1JPT00vNGNmODI4ODAtN2ZkNy0xMWViLWI0MDgtN2RmNzQ0MDlmMmU5"
-config['TESTING_ROOM'] = "Y2lzY29zcGFyazovL3VzL1JPT00vYzM2YjQ2MTAtODBiNy0xMWViLThmMDItNjc3MTRkYWU1NGU4"
-
-config["PRODUCTION_ROOM_TITLE"] = "Programmability CTF - Day 1 - Team 1"
-config["TESTING_ROOM_TITLE"] = "Patrick Holse appreciation club"
